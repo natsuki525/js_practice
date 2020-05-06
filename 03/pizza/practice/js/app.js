de5af3604c114a8713c47498af10e49aa9bfd3df
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-05-06 22:46:13 +0900

    [finish] chapter3

--- 03/pizza/practice/js/app.js
+++ 03/pizza/practice/js/app.js
@@ -1,6 +1,9 @@
 let budget = prompt('所持金を数字で入力してください。');
 budget = parseFloat(budget);
-if (budget >= 1500) {
+
+let isHungry = confirm('お腹は空いてますか？');
+
+if (budget >= 1500 && isHungry) {
     alert('ピザを注文しました。');
 } else if (budget >= 500){
 	alert('ポテトを注文しました。');
